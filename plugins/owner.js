9ad70f1cf46a99849395c8d1be9be37eba7d1e02
userafake <89152959+userafake@users.noreply.github.com>
2021-09-14 13:08:14 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -36,10 +36,10 @@ else if (Config.WORKTYPE == 'public') {
         }
 
         if (Config.PLK == 'default') {
-            await message.client.sendMessage(message.jid,'Pikachu Bot created by *Ameer Suhail*' , MessageType.text);
+            await message.client.sendMessage(message.jid,'THAKUDU bot was created by *Jojit💞\n :https://wa.link/v2d5vu*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS*', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.PLK + '*HE IS MY BOSS;My creator is Jojit💞\n :https://wa.link/v2d5vu*', MessageType.text);
         }
     }));
 }
