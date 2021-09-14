4af204678101913d0c1f7cd71efc571474133dbb
userafake <89152959+userafake@users.noreply.github.com>
2021-09-14 17:21:33 +0530

    Update jojit.js

--- plugins/jojit.js
+++ plugins/jojit.js
@@ -10,7 +10,7 @@ Asena.addCommand({pattern: 'jojit', fromMe: false, desc: Lang.WP}, (async (messa
     var r_text = new Array ();
     
     
-    r_text[0] = "https://wallpaperaccess.com/full/1371433.jpg";
+    r_text[0] = "https://textpro.me/images/user_image/2021/09/61408bce7c0a2.jpg-s1041-b1";
     
     
     var i = Math.floor(1*Math.random())
@@ -18,11 +18,14 @@ Asena.addCommand({pattern: 'jojit', fromMe: false, desc: Lang.WP}, (async (messa
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━💞Jojit💞━━━━━⛦
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 𝐛𝐨𝐭.
-            *T H A K U D U*
+   
+    
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 𝐛𝐨𝐭.\n
+     *T H A K U D U*\n
+         createdby:*Jojit*\n
+ Whatsapp my boss:\n
+ https://wa.link/v2d5vu
 _________________________________
-
-▣▣▣▣▣▣▣JOJIT▣▣▣▣▣▣▣
 `}) 
 
 }));
