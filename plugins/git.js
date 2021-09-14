3545acc708558e9cf3907e716ffa515ac9c2d257
userafake <89152959+userafake@users.noreply.github.com>
2021-09-14 12:59:46 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,19 +11,17 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+  r_text[0] = "https://wallpaperaccess.com/full/1371433.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Ameer Suhail*
-*Creator number : wa.me/919946432377?text=Hi%20Ameer%20Suhail.%20*
-*Bot setting video : https://youtu.be/_D4ZYuUSXjs*
-*Githublink (Setup)  :    https://github.com/ameer-kallumthodi/pikachu*
-*Audio commads :   https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
-*Sticker commads : https://github.com/ameer-kallumthodi/pikachu/tree/master/uploads*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is Jojit💞*
+*Creator number : https://wa.link/v2d5vu*
+*Githublink (Setup)  :    https://github.com/userafake/thakudu*
+
 `}) 
 
 }));
